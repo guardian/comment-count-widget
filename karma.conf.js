50a1700c8e0f1db51779586288995a143b080119
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-07-21 10:53:26 +0100

    coverage only on single run

--- karma.conf.js
+++ karma.conf.js
@@ -75,11 +75,11 @@ module.exports = function(config) {
         concurrency: Infinity,
 
         rollupPreprocessor: {
-            plugins: [
+            plugins: (config.singleRun ? [
                 istanbul({
                     exclude: ['test/**/*.js', 'node_modules/**/*.js']
                 })
-            ].concat(rollupOpts.plugins),
+            ] : []).concat(rollupOpts.plugins),
             // will help to prevent conflicts between different tests entries
             format: 'iife',
             sourceMap: 'inline'
